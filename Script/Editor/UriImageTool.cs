3c34018dec309d33d421710b94ae3581414e2391
0kk470 <2549366978@qq.com>
2022-05-20 15:59:13 +0800

    update UriImageTool

--- Script/Editor/UriImageTool.cs
+++ Script/Editor/UriImageTool.cs
@@ -9,15 +9,30 @@ namespace SaltyfishKK.UriImage.Editor
 {
     public static class UriImageTool
     {
+        private static readonly string SettingFileDirectory = Application.dataPath + "/Resources/UriImage";
+
 
         [MenuItem("Tools/UriImage/Create New UriImage Setting")]
         public static void CreateNewSetting()
         {
-
-            var asset = ScriptableObject.CreateInstance(typeof(UriImage_Setting));
+            var t = typeof(UriImage_Setting);
+            var GuidPaths = AssetDatabase.FindAssets("t:"+ t.FullName);
+            foreach(var guid in GuidPaths)
+            {
+                var path = AssetDatabase.GUIDToAssetPath(guid);
+                if(path.Contains("Resources"))
+                {
+                    EditorUtility.DisplayDialog("Notice", "UriImage Setting is already existed, path:" + path, "ok");
+                    return;
+                }
+            }
+            if(!Directory.Exists(SettingFileDirectory))
+            {
+                Directory.CreateDirectory(SettingFileDirectory);
+            }
+            var asset = ScriptableObject.CreateInstance(t);
             AssetDatabase.CreateAsset(asset, "Assets/Resources/UriImage/UriImageSetting.asset");
             AssetDatabase.SaveAssets();
-
             Selection.activeObject = asset;
         }
     }
